134e108cf89ca0e11d0ff6b48e59181e6a004d61
lakshmananal-cpu <lakshmanan.al@gmail.com>
2026-07-12 00:19:56 +0100

    fix: keep Blog link visible in nav on narrow/mobile screens

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
       <Link href="/" className="nav-brand">Laks Alagappan</Link>
       <ul className="nav-links">
         <li><Link href="/#speaking">Writing</Link></li>
-        <li><Link href="/blog">Blog</Link></li>
+        <li className="nav-keep"><Link href="/blog">Blog</Link></li>
         <li><Link href="/#talks">Speaking</Link></li>
         <li><Link href="/#ventures">Building</Link></li>
         <li><Link href="/#contact" className="nav-cta">Get in touch</Link></li>
